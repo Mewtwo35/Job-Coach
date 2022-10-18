9f9e06cc24a7b2a693e18c8cd0d7cb857c4326a5
Adam Seery <102638662+couloirr@users.noreply.github.com>
2022-10-18 12:39:16 -0600

    feat: completed routing and controllers for database with commenting

--- src/server/controllers/algoPGController.ts
+++ src/server/controllers/algoPGController.ts
@@ -4,11 +4,11 @@ const db = require('../models/algoModels');
 type AlgoPGController = {
   getAllAlgos: RequestHandler;
   getAlgo: RequestHandler;
-  //   getAlgosCat: RequestHandler;
+  getAlgosCat: RequestHandler;
 };
 export const algoPGController: AlgoPGController = {
   /**
-   *
+   *Finds and returns ALL stored algos
    * @returns array of objects containing all stored algorithms
    */
   getAllAlgos: async (
@@ -28,9 +28,9 @@ export const algoPGController: AlgoPGController = {
     }
   },
   /**
-   *
+   *finds and returns a single algo db object using the name on request query
    * @param req as algoName on query
-   * @returns object with
+   * @returns object with returned query algo data fields
    */
   getAlgo: async (
     req: Request,
@@ -42,6 +42,26 @@ export const algoPGController: AlgoPGController = {
       const queryString = `SELECT * FROM "public"."algos" WHERE name = '${algoName}'`;
       const result = await db.query(queryString);
       res.locals.algos = result.rows[0];
+      return next();
+    } catch (err) {
+      return next(err);
+    }
+  },
+  /**
+   *Finds and returns ALL stored algos by category
+   * @param req as algoCat on query
+   * @returns array of objects containing all stored algorithms
+   */
+  getAlgosCat: async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> => {
+    try {
+      const { algoCat } = req.query;
+      const queryString = `SELECT * FROM "public"."algos" WHERE category = '${algoCat}'`;
+      const result = await db.query(queryString);
+      res.locals.algos = result.rows;
       return next();
     } catch (err) {
       return next(err);
--- src/server/routers/routes.ts
+++ src/server/routers/routes.ts
@@ -4,17 +4,38 @@ const router = express.Router();
 import { Request, Response } from 'express';
 import editorController from '../controllers/editorController';
 import { algoPGController } from '../controllers/algoPGController';
-//get dashboard page
+/**
+ * gets all algos from database
+ * @params none
+ * @returns array of algo objects
+ */
 router.get(
-  '/dashboard',
+  '/allAlgos',
   algoPGController.getAllAlgos,
   (req: Request, res: Response) => {
     return res.status(200).send(res.locals.algos);
   }
 );
+/**
+ * get single algorithm using db name on query
+ * @params name of algo as query algoName: name
+ * @returns object with all db fields for found algo
+ */
 router.get('/algo', algoPGController.getAlgo, (req: Request, res: Response) => {
   return res.status(200).send(res.locals.algos);
 });
+/**
+ * gets algorithms using db category on query
+ * @params category of algos as query algoCat: categoryname
+ * @returns array of algo objects
+ */
+router.get(
+  '/algoCat',
+  algoPGController.getAlgosCat,
+  (req: Request, res: Response) => {
+    return res.status(200).send(res.locals.algos);
+  }
+);
 
 //get code editor page
 router.get('/editor', (req: Request, res: Response) => {
