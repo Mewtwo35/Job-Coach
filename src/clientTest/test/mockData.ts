1a92f483314e07968c8669e6d49e0d815e1ebac3
Adam Seery <102638662+couloirr@users.noreply.github.com>
2022-10-19 12:06:13 -0600

    added test for fizzbuzz

--- src/clientTest/test/mockData.ts
+++ src/clientTest/test/mockData.ts
@@ -10,12 +10,19 @@ export const mockData = {
       'Create a function "sum" that accepts two numbers and returns the sum',
   },
   fizzbuzz: {
-    startFunc: `function fizzbuzz(a, b) {  } document.write(sum())`,
-    testStatement: `describe('sum', function () {
-      it('should return sum of arguments', function () {
-        chai.expect(sum(1, 2)).to.equal(3);
+    startFunc: `function fizzbuzz(num) {  } document.write(fizzbuzz())`,
+    testStatement: `describe('fizzbuzz', function () {
+      it('should work for multiples of 3 and 5', function () {
+        const arr = fizzbuzz(31)
+        chai.expect(arr.length).toBe(31);
+        chai.expect(arr[0]).toBe(1);
+        chai.expect(arr[6]).toBe(7);
       });
     });`,
-    prompt: 'fizz or buzz who knows',
+    prompt: `Write a function that returns an array containing the numbers 1 to NUM.
+
+    Put "fizz" in place of numbers divisble by 3 but not by 5,
+    "buzz" in place of numbers divisble by 5 but not by 3,
+    and "fizzbuzz" in place of numbers divisble by both 3 and 5.`,
   },
 };
