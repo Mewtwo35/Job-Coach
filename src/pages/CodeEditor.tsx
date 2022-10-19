8cfc1d4e091f38669c89afc04dbd61793e891fe5
Adam Seery <102638662+couloirr@users.noreply.github.com>
2022-10-18 22:33:59 -0600

    code editor is outputting result of returned function

--- src/pages/CodeEditor.tsx
+++ src/pages/CodeEditor.tsx
@@ -1,42 +1,65 @@
 import * as React from 'react';
 import CodeMirror from '@uiw/react-codemirror';
+// import CodeMirror from 'codemirror';
 import { javascript } from '@codemirror/lang-javascript';
 import Button from '@mui/material/Button';
+import { useEffect } from 'react';
 
-class CodeEditor extends React.Component {
-    
-    onChange = React.useCallback((value:any, viewUpdate:any) => {
-        console.log('value:', value);
-    }, []);
+const CodeEditor = () => {
+  let storedVal: any;
+  const onChange = React.useCallback((value: any, viewUpdate: any) => {
+    console.log('value:', value);
+    storedVal = value;
+    console.log(storedVal, 'stored val');
+  }, []);
+  document.write = function (s) {
+    var scripts = document.getElementsByTagName('script');
+    var lastScript = scripts[scripts.length - 1];
+    lastScript.insertAdjacentHTML('beforebegin', s);
+  };
+  function handleClick(e: any) {
+    e.preventDefault();
+    console.log(document);
+    const s = document.createElement('script');
+    const pConsole = document.querySelector('.pconsole');
+    const consoleEl = document.querySelector('.console');
+    s.setAttribute('id', 'chalfunction');
+    s.textContent = storedVal; //inne
+    console.log(s);
+    consoleEl?.appendChild(s);
+    pConsole?.remove();
+    // consoleEl?.append("<p class='pconsole indent'>" + s + ' </p>');
+  }
+  return (
+    <div>
+      <h2>CodeMirror</h2>
+      <div className="console">
+        {/* <p className="pconsole">/**</p>
+        <p className="pconsole indent">* Function out put will go here.</p>
+        <p className="pconsole indent">* /</p> */}
+      </div>
+      <script id="script" type="text/javascript"></script>
 
-    render() {
-        return (
-          <div>
-            <h2>CodeMirror</h2>
-                
-                <Button variant="contained">Logout</Button>
+      <div className="prompt"></div>
 
-                <div className="prompt">
-                </div>
+      <div className="list-of-topics"></div>
 
-                <div className="list-of-topics">
-                </div>
-
-                <div className="codemirror">
-                    <CodeMirror
-                        value="console.log('hello world!');"
-                        height="200px"
-                        extensions={[javascript({ jsx: true })]}
-                        onChange={this.onChange}
-                    />
-                </div>
-
-                <Button variant="contained">Submit</Button>
-
-          </div>
-          
-        )
-      }
+      <div className="codemirror">
+        <CodeMirror
+          value="function hello(){
+document.write('hello')
 }
+hello();"
+          height="200px"
+          //   extensions={[javascript({ jsx: true })]}
+          onChange={onChange}
+        />
+      </div>
+
+      <Button variant="contained">Submit</Button>
+      <button onClick={handleClick}>Run</button>
+    </div>
+  );
+};
 
 export default CodeEditor;
