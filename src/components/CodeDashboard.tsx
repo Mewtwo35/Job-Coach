bb8508f6bd9ac667f12ad84c19753f471f8f3b75
Adam Seery <102638662+couloirr@users.noreply.github.com>
2022-10-19 13:22:56 -0600

    minor styling fixes to code editor

--- src/components/CodeDashboard.tsx
+++ src/components/CodeDashboard.tsx
@@ -3,12 +3,10 @@ import CodeEditor from './CodeEditor';
 
 const CodeDashboard = () => {
   return (
-    <div id='code-dashboard'>
-      <div id='sidebar'>
-        <span>PROMPT HERE</span>
-        <button>{'<'}  All Challenges</button>
-      </div>
-      <CodeEditor/>
+    <div id="code-dashboard">
+      <div id="sidebar">{/* <span>PROMPT HERE</span> */}</div>
+      <button>{'<'} All Challenges</button>
+      <CodeEditor />
     </div>
   );
 };
