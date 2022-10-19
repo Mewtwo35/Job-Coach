8442e1318fa7e008a670dbe1b0d2c8d0ee0da332
Kara Chisholm <kkchisholm@gmail.com>
2022-10-18 21:11:49 -0400

    browserTest folder
    Co-authored-by: Nicholas Echevarria <nick-echevarria@users.noreply.github.com>

--- src/pages/AlgoDashboard.tsx
+++ src/pages/AlgoDashboard.tsx
@@ -1,9 +1,5 @@
 import * as React from 'react';
 import CategoriesComponent from './CategoriesComponent';
-
-
-
-
 class AlgoDashboard extends React.Component {
   render() {
     return (
--- src/server/controllers/editorController.ts
+++ src/server/controllers/editorController.ts
@@ -2,7 +2,9 @@ import { EditorController } from '../../types'
 
 const editorController: EditorController = {
   testAnswer: (req, res, next) => {
-    console.log('TEST ANSWER FUNC');
+    //1. write submitted code to a new file using fs.writeFileSync,
+    //making sure to title it <algoName>.test.js
+    //2. use a .sh script to run a jest test within 
     next();
   },
 };
